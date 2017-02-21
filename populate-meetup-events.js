61012f8c34a0bc3ca31477d6f33004700efe1267
Andy Carson <andrew.carson@silverpond.com.au>
2017-02-21 20:46:05 +1100

    clear out any exiting events before pulling from meetup.com

--- populate-meetup-events.js
+++ populate-meetup-events.js
@@ -1,34 +1,36 @@
-const Client = require('node-rest-client').Client
-const fs = require('fs')
-const Mustache = require('mustache')
-const _ = require('lodash')
+const _ = require('lodash');
+const Client = require('node-rest-client').Client;
+const fs = require('fs-extra');
+const Mustache = require('mustache');
 
 const meetups = [
   {
     apiLink: 'https://api.meetup.com/Melbourne-Haskell-Users-Group/events?photo-host=public&page=20&sig_id=144437602&sig=4710da284f67f75fe5f362237012caf911da03c7',
-    host: 'Lyndon Maydwell',
+    host: 'Lyndon Maydwell'
   },
   {
     apiLink: 'https://api.meetup.com/Melbourne-Functional-User-Group-MFUG/events?photo-host=public&page=20&sig_id=144437602&sig=98a300693323377db293e89b49519cab359f8d74',
-    host: 'Logan Campbell',
+    host: 'Logan Campbell'
   },
   {
     apiLink: 'https://api.meetup.com/clj-melb/events?photo-host=public&page=20&sig_id=144437602&sig=28fe70a7678c63886bf70e0e5ebdae498e44ee48',
-    host: 'James Sofra',
+    host: 'James Sofra'
   },
   {
     apiLink: 'https://api.meetup.com/The-Melbourne-Maths-and-Science-Meetup/events?photo-host=public&page=20&sig_id=144437602&sig=8aa8c61e748bb5215c6da8a05de1172556de5ec6',
-    host: 'Noon Silk',
-  },
-]
+    host: 'Noon Silk'
+  }
+];
 
 const client = new Client();
+const eventDir = 'pages/meetup-events/';
 
+// Create event .md file
 const createEvent = (event, host) => {
-  const template = fs.readFileSync('event.md.template', { encoding: 'utf8' })
+  const template = fs.readFileSync('event.md.template', { encoding: 'utf8' });
 
   fs.writeFileSync(
-    `pages/meetup-events/${event.time}.md`,
+    `${eventDir}${event.time}.md`,
     Mustache.render(template, {
       title: event.name,
       slug: `${_.kebabCase(event.name)}-${event.time}`,
@@ -36,18 +38,22 @@ const createEvent = (event, host) => {
       venue: event.venue,
       body: event.description,
       attendLink: event.link,
-      host: host,
+      host: host
     })
-  )
-}
+  );
+};
 
+// Clear out any existing generated .md event files
+if (fs.existsSync(eventDir)) {
+  fs.removeSync(eventDir);
+}
+fs.mkdirSync(eventDir);
+
+// Get events from meetup.com and create event .md files
 meetups.forEach(meetup => {
-  client.get(
-    meetup.apiLink,
-    events => {
-      events.forEach(event => {
-        createEvent(event, meetup.host)
-      })
-    }
-  )
-})
+  client.get(meetup.apiLink, events => {
+    events.forEach(event => {
+      createEvent(event, meetup.host);
+    });
+  });
+});
