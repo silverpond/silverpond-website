d503b9200b5349d07ab8b23b4fa3868d6c5d175d
Andy Carson <andrew.carson@silverpond.com.au>
2017-03-05 11:13:24 +1100

    don't need Inner if using Section

--- pages/about.js
+++ pages/about.js
@@ -9,7 +9,6 @@ import { textBlock } from '../lib/styles';
 import { filterPages, imagePath } from '../lib/utilities';
 
 import Header from '../components/Header';
-import Inner from '../components/Inner';
 import PersonSmall from '../components/PersonSmall';
 import SplashImage from '../components/SplashImage';
 import { ColWrapper, Col } from '../components/Grid';
@@ -54,67 +53,65 @@ const About = (
     <div>
       <Header />
       <SplashImage src="/images/team.jpg" />
-      <Section>
-        <Inner size="small">
-          <Title>
-            We are Silverpond!
-          </Title>
-          <Body>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-              enim mi, vulputate nec tincidunt quis, finibus id ex. Quisque
-              venenatis rhoncus odio, eu ornare tortor pretium sit amet. Sed
-              tincidunt maximus felis a dictum. Donec eu vestibulum sapien. Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse enim
-              mi, vulputate nec tincidunt quis, finibus id ex. Quisque venenatis
-              rhoncus odio, eu ornare tortor pretium sit amet. Sed tincidunt
-              maximus felis a dictum. Donec eu vestibulum sapien.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-              enim mi, vulputate nec tincidunt quis, finibus id ex. Quisque
-              venenatis rhoncus odio, eu ornare tortor pretium sit amet. Sed
-              tincidunt maximus felis a dictum. Donec eu vestibulum sapien. Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse enim
-              mi, vulputate nec tincidunt quis, finibus id ex. Quisque venenatis
-              rhoncus odio, eu ornare tortor pretium sit amet. Sed tincidunt
-              maximus felis a dictum. Donec eu vestibulum sapien.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-              enim mi, vulputate nec tincidunt quis, finibus id ex. Quisque
-              venenatis rhoncus odio, eu ornare tortor pretium sit amet. Sed
-              tincidunt maximus felis a dictum. Donec eu vestibulum sapien. Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse enim
-              mi, vulputate nec tincidunt quis, finibus id ex. Quisque venenatis
-              rhoncus odio, eu ornare tortor pretium sit amet. Sed tincidunt
-              maximus felis a dictum. Donec eu vestibulum sapien.
-            </p>
-          </Body>
+      <Section size="small">
+        <Title>
+          We are Silverpond!
+        </Title>
+        <Body>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            enim mi, vulputate nec tincidunt quis, finibus id ex. Quisque
+            venenatis rhoncus odio, eu ornare tortor pretium sit amet. Sed
+            tincidunt maximus felis a dictum. Donec eu vestibulum sapien. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse enim
+            mi, vulputate nec tincidunt quis, finibus id ex. Quisque venenatis
+            rhoncus odio, eu ornare tortor pretium sit amet. Sed tincidunt
+            maximus felis a dictum. Donec eu vestibulum sapien.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            enim mi, vulputate nec tincidunt quis, finibus id ex. Quisque
+            venenatis rhoncus odio, eu ornare tortor pretium sit amet. Sed
+            tincidunt maximus felis a dictum. Donec eu vestibulum sapien. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse enim
+            mi, vulputate nec tincidunt quis, finibus id ex. Quisque venenatis
+            rhoncus odio, eu ornare tortor pretium sit amet. Sed tincidunt
+            maximus felis a dictum. Donec eu vestibulum sapien.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            enim mi, vulputate nec tincidunt quis, finibus id ex. Quisque
+            venenatis rhoncus odio, eu ornare tortor pretium sit amet. Sed
+            tincidunt maximus felis a dictum. Donec eu vestibulum sapien. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse enim
+            mi, vulputate nec tincidunt quis, finibus id ex. Quisque venenatis
+            rhoncus odio, eu ornare tortor pretium sit amet. Sed tincidunt
+            maximus felis a dictum. Donec eu vestibulum sapien.
+          </p>
+        </Body>
 
-          <SubTitle>
-            Meet the team
-          </SubTitle>
+        <SubTitle>
+          Meet the team
+        </SubTitle>
 
-          {chunk(people, 3).map(row => {
-            return (
-              <PeopleRow key={hash(row)}>
-                {row.map(person => {
-                  return (
-                    <Col span="4" key={hash(person)}>
-                      <PersonSmall
-                        name={person.data.name}
-                        description={person.data.role}
-                        image={imagePath(person.path, person.data.image)}
-                      />
-                    </Col>
-                  );
-                })}
-              </PeopleRow>
-            );
-          })}
+        {chunk(people, 3).map(row => {
+          return (
+            <PeopleRow key={hash(row)}>
+              {row.map(person => {
+                return (
+                  <Col span="4" key={hash(person)}>
+                    <PersonSmall
+                      name={person.data.name}
+                      description={person.data.role}
+                      image={imagePath(person.path, person.data.image)}
+                    />
+                  </Col>
+                );
+              })}
+            </PeopleRow>
+          );
+        })}
 
-        </Inner>
       </Section>
     </div>
   );
