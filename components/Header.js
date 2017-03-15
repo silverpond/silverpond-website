fe05164e4e8510cb91e99003dcd3d2bf2db7b990
Andy Carson <andrew.carson@silverpond.com.au>
2017-03-16 08:38:47 +1100

    bottom border for header

--- components/Header.js
+++ components/Header.js
@@ -3,12 +3,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import { prefixLink } from 'gatsby-helpers';
+import { palette } from '../lib/settings';
 
 import Nav from '../components/Nav';
 import LogoLink from '../components/LogoLink';
 
 const Container = styled.div`
   align-items: center;
+  border-bottom: solid .25rem ${palette.red.base};
   display: flex;
   justify-content: space-between;
   padding: 2rem 3rem;
