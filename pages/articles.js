59031c8cc4fc09e0d0a8df44846771e4942ccba2
Andy Carson <andrew.carson@silverpond.com.au>
2017-03-22 18:15:56 +1100

    filter out article drafts

--- pages/articles.js
+++ pages/articles.js
@@ -27,7 +27,9 @@ const Articles = (
     route: Object,
   },
 ) => {
-  const articles = filterPagesByCategory(pages, 'articles');
+  const articles = filterPagesByCategory(pages, 'articles').filter(
+    article => !article.data.draft === true,
+  );
   const featuredArticle = findFeaturedPages(articles)[0];
   const nonFeaturedArticles = filterNot(articles, [featuredArticle]).reverse();
 
