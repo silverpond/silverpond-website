15b6dac70e07310258019e09d66b044323628a7c
Andy Carson <andrew.carson@silverpond.com.au>
2017-04-11 17:20:16 +1000

    Inner width to be 100% below size threshold

--- components/Inner.js
+++ components/Inner.js
@@ -1,21 +1,31 @@
+// @flow
 import styled from 'styled-components';
 
-type Size = 'medium';
+type Size = 'medium' | 'small';
 
-const getSize = (size: Size): string => {
+const widthStyles = (width: string): string => {
+  return `
+    width: ${width};
+    @media (max-width: ${width}) {
+      width: 100%;
+    }
+  `;
+};
+
+const getWidths = (size: Size): string => {
   switch (size) {
     case 'small':
-      return '700px';
+      return widthStyles('700px');
     case 'medium':
-      return '950px';
+      return widthStyles('950px');
     default:
-      return '1175px';
+      return widthStyles('1175px');
   }
 };
 
 const Inner = styled.div`
+  ${props => getWidths(props.size)}
   margin: 0 auto;
-  width: ${props => getSize(props.size)};
 `;
 
 export default Inner;
