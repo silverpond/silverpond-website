0a7267f1c20f14b204ede4e1bb958572efc91912
Andy Carson <andrew.carson@silverpond.com.au>
2017-04-27 17:28:12 +1000

    responsive styles for splashImage

--- components/SplashImage.js
+++ components/SplashImage.js
@@ -2,12 +2,17 @@
 // Imports - config
 import styled from 'styled-components';
 import { settings as mastHeadSettings } from '../components/MastHead';
+import { media } from 'lib/styles';
 
 // Component
 const SplashImage = styled.img`
   height: ${mastHeadSettings.height}
   object-fit: cover;
   width: 100%;
+
+  ${media.small`
+    height: 20rem;
+  `}
 `;
 
 export default SplashImage;
